e03801a762026deb2689ccc4e2364f8aa7df15ce
a104187 <a104187@alunos.uminho.pt>
2025-02-12 23:28:13 +0000

    TPC1 acrescentado

---
+++ TPC1/README.md
@@ -0,0 +1,28 @@
+# TPC1 - Somas on/off
+
+## 📌 Descrição
+O TPC1 consiste em desenvolver um programa python que percorre um texto caractere por caractere, somando todas as sequências de dígitos encontradas. 
+- Se a palavra "off" for detetada (independentemente de maiúsculas/minúsculas), a soma é desativada.
+- Se a palavra "on" for encontrada, a soma é reativada.
+- Quando o caractere "=" aparece, o programa imprime o total acumulado até esse momento.
+- Além disso, o código lida corretamente com números negativos, verificando se um "-" precede um dígito.
+
+## 📌 Exemplo de Entrada e Saída
+
+**Entrada:**
+```
+hfy3hud=jdon52=hdoff3jon-2=f
+```
+
+**Saída:**
+```
+3
+55
+53
+```
+
+- **Nome** : Marco António Fernandes Brito
+
+ ![104187](104187.png)
+
+
---
+++ TPC1/main.py
@@ -0,0 +1,44 @@
+def contador_on_off(text):
+    soma_total= 0
+    soma_ativa= True
+    num_atual=""
+    i =0
+
+    while i<len(text):
+        char=text[i]
+
+        if char.isdigit():
+            num_atual += char
+        
+        else:
+            if num_atual:
+                if soma_ativa:
+                    soma_total += int(num_atual)
+                num_atual = ""  
+
+            if text[i] == "=":
+                print(soma_total)
+        
+        if text[i:i+3].lower()=="off":
+            soma_ativa=False
+            i +=2
+
+        elif text[i:i+2].lower()=="on":
+            soma_ativa=True
+            i +=1
+
+        elif char == "-":
+            if i + 1 < len(text) and text[i + 1].isdigit():
+                num_atual = "-"  
+
+        i += 1
+    
+    if num_atual and num_atual not in ["-"] and soma_ativa:
+        soma_total += int(num_atual)
+
+
+# hfy3hud=jdon52=hdoff3jon-2=f
+
+text= input("Escreva frase pretendida:")
+contador_on_off(text)
+                
